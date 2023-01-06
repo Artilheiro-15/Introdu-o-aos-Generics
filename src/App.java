d545988267ed3b5af896bda88a87ab0ff35853ea
Atila jr <atilla.junior33@gmail.com>
2023-01-06 10:54:04 -0300

    Funcionando para String

--- src/App.java
+++ src/App.java
@@ -11,18 +11,18 @@ public class App {
 
     // PrintService ps = new PrintService();
 
-    PrintService<Integer> ps = new PrintService<>();
+    PrintService<String> ps = new PrintService<>();
 
     System.out.print("How many values? ");
     int n = sc.nextInt();
 
     for (int i = 0; i < n; i++) {
-      Integer value = sc.nextInt();
+      String value = sc.next();
       ps.addValue(value);
     }
 
     ps.print();
-    Integer x = ps.first();
+    String x = ps.first();
     System.out.println("First: " + ps.first());
 
     System.out.println("================FIM==================");
