0f83514dae6a6150340794bbb1e485e60eb329ca
Atila jr <atilla.junior33@gmail.com>
2023-01-06 10:50:46 -0300

    Funcionando para inteiro

--- src/App.java
+++ src/App.java
@@ -11,12 +11,10 @@ public class App {
 
     // PrintService ps = new PrintService();
 
-    PrintService ps = new PrintService();
+    PrintService<Integer> ps = new PrintService<>();
 
     System.out.print("How many values? ");
     int n = sc.nextInt();
-
-    ps.addValue("Maria");
 
     for (int i = 0; i < n; i++) {
       Integer value = sc.nextInt();
@@ -24,7 +22,7 @@ public class App {
     }
 
     ps.print();
-    Integer x = (Integer) ps.first();
+    Integer x = ps.first();
     System.out.println("First: " + ps.first());
 
     System.out.println("================FIM==================");
--- src/services/PrintService.java
+++ src/services/PrintService.java
@@ -3,15 +3,16 @@ package services;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PrintService {
+//Agora a minha class esta parametrizada com um certo tipo T
+public class PrintService<T> {
 
-  private List<Object> list = new ArrayList<>();
+  private List<T> list = new ArrayList<>();
 
-  public void addValue(Object value) {
+  public void addValue(T value) {
     list.add(value);
   }
 
-  public Object first() {
+  public T first() {
     if (list.isEmpty()) {
       throw new IllegalStateException("List is empty");
     }
