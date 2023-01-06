45e5e15346a72a33fbc227e2a951a0342854fc95
Atila jr <atilla.junior33@gmail.com>
2023-01-06 10:43:40 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+import services.PrintService;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+    System.out.println("==================================");
+
+    // PrintService ps = new PrintService();
+
+    PrintService ps = new PrintService();
+
+    System.out.print("How many values? ");
+    int n = sc.nextInt();
+
+    ps.addValue("Maria");
+
+    for (int i = 0; i < n; i++) {
+      Integer value = sc.nextInt();
+      ps.addValue(value);
+    }
+
+    ps.print();
+    Integer x = (Integer) ps.first();
+    System.out.println("First: " + ps.first());
+
+    System.out.println("================FIM==================");
+    sc.close();
+  }
+}
